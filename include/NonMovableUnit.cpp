4b9aee5845ab41300d1174b79d3bad8525774bd7
qjx <996543486@qq.com>
2023-07-24 14:05:46 +0800

    固定单位类定义

--- include/NonMovableUnit.cpp
+++ include/NonMovableUnit.cpp
@@ -1,38 +1,32 @@
-// NonMovableUnit.cpp
 #include "NonMovableUnit.h"
 
-NonMovableUnit::NonMovableUnit() {
-    // 在构造函数中生成唯一的ID
-    id = generateUniqueID();
-    positionX = 0.0;
-    positionY = 0.0;
-    positionZ = 0.0;
-    velocity = 0.0;
-    status = true; // 假设true表示活动，false表示死亡
-    communication_distance = 0.0; // 初始化通讯距离为0
-    camp = 0; // 初始化阵营归属为0（可以根据需求定义阵营标识）
+// 构造函数
+NonMovableUnit::NonMovableUnit(int id)
+    : ID(id), positionX(0.0), positionY(0.0), positionZ(0.0), status(true), communication_distance(0.0), camp(0) {}
+
+// 析构函数
+NonMovableUnit::~NonMovableUnit() {}
+
+// 属性的getter方法实现
+int NonMovableUnit::getID() const {
+    return id;
 }
 
-void NonMovableUnit::move() {
-    // 实现move方法，根据需要进行移动逻辑
-    // 对于不可移动的对象，该方法可以为空，或者不做任何操作
+double NonMovableUnit::getPositionX() const {
+    return positionX;
 }
 
-void NonMovableUnit::change_speed() {
-    // 实现change_speed方法，根据需要修改速度逻辑
-    // 为简单起见，我们将速度设置为固定值10单位
-    velocity = 10.0;
+double NonMovableUnit::getPositionY() const {
+    return positionY;
 }
 
-// 生成唯一的UID方法的实现（根据需求自行实现）
-int NonMovableUnit::generateUniqueID() {
-    // 实现生成唯一ID的逻辑，例如使用时间戳、随机数生成器等方式
-    // 这里简化为返回固定的ID 1
-    return 1;
+double NonMovableUnit::getPositionZ() const {
+    return positionZ;
 }
 
-// 属性的getter和setter方法的实现
-// ... 省略其他属性的getter和setter方法实现 ...
+bool NonMovableUnit::getStatus() const {
+    return status;
+}
 
 double NonMovableUnit::getCommunicationDistance() const {
     return communication_distance;
@@ -44,6 +38,17 @@ int NonMovableUnit::getCamp() const {
 
 const std::vector<int>& NonMovableUnit::getRelatedObjectIDs() const {
     return related_object_ids;
+}
+
+// 属性的setter方法实现
+void NonMovableUnit::setPosition(double x, double y, double z) {
+    positionX = x;
+    positionY = y;
+    positionZ = z;
+}
+
+void NonMovableUnit::setStatus(bool status) {
+    this->status = status;
 }
 
 void NonMovableUnit::setCommunicationDistance(double distance) {
--- include/NonMovableUnit.h
+++ include/NonMovableUnit.h
@@ -6,8 +6,8 @@
 
 class NonMovableUnit {
 public:
-    // 默认构造函数
-    NonMovableUnit(); 
+    // 构造函数
+    NonMovableUnit(int id); 
 
     // 析构函数
     ~NonMovableUnit();
@@ -29,15 +29,12 @@ public:
     void addRelatedObjectID(int objectID);
 
 private:
-    // 生成唯一的UID方法（可以根据需求自行实现）
-    int generateUniqueID();
-
     // 属性
-    int id;                   // 对象的唯一标识符
+    int ID;                   // 对象的唯一标识符
     double positionX;        // 对象的当前X坐标
     double positionY;        // 对象的当前Y坐标
     double positionZ;        // 对象的当前Z坐标
-    bool status;             // 对象的状态（活动/死亡等）
+    bool status;             // 对象的状态（活动/死亡）
     double communication_distance; // 通讯距离
     int camp;             // 阵营归属
     std::vector<int> related_object_ids; // 与其有关联对象的ID列表
